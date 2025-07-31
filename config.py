690a0f0b5d3c9ff1a0d58311f431257afe67a126
giordana79 <34681234+giordana79@users.noreply.github.com>
2025-07-31 13:57:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
     MATRIX_HOMESERVER_URL = os.getenv('MATRIX_HOMESERVER_URL', 'https://matrix.sito.it')
     MATRIX_USER = os.getenv('MATRIX_USER', '@giordana:matrix.sito.it')
-    MATRIX_PASSWORD = os.getenv('MATRIX_PASSWORD', 'Giord@n@')
+    MATRIX_PASSWORD = os.getenv('MATRIX_PASSWORD', 'xxxxxxxx')
     MATRIX_ROOM_ID = os.getenv('MATRIX_ROOM_ID', 'xxxxxxxxxx:matrix.sito.it')
   
 
